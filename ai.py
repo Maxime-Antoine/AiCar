24e2cd8ec2a542b464a5866db71715dd34c49b7f
Maxime Antoine <maxime.antoine2@gmail.com>
2017-10-02 00:32:29 +0800

    Finished creating environment

---
+++ ai.py
@@ -0,0 +1,12 @@
+from random import randint
+
+class Dqn(object):
+    
+    def __init__(self, *args):
+        pass
+
+    def update(*args):
+        return randint(0, 2)
+
+    def score(*args):
+        pass
