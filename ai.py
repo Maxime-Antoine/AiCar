fb207ca07cbac3d792decdb45f6f55a3f307d572
Maxime <maxime.antoine2@gmail.com>
2017-10-02 18:20:07 +0800

    Added Network and ReplayMemory

--- ai.py
+++ ai.py
@@ -1,7 +1,47 @@
-from random import randint
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+
+class Network(nn.Module):
+
+    def __init__(self, input_size, nb_actions):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_actions = nb_actions
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_actions)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state)) # rectifier function
+        q_values = self.fc2(x)
+        return q_values
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
 
 class Dqn(object):
-    
+
     def __init__(self, *args):
         pass
 
